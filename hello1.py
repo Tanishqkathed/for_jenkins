25a2ea26ad8d94ea2a81e58a19f427c81d0ec217
Tanishq kanthed <94047738+Tanishqkathed@users.noreply.github.com>
2026-05-27 16:42:41 +0530

    Add new print statement for Jenkins pipeline check

--- hello1.py
+++ hello1.py
@@ -1,3 +1,5 @@
+
 print("Hello I am writing inside CMD ! Anyways Hello from Jenkins ... Tanishq!")
 print("This is updated version .. will this show on cmd ?")
 print("This is another update will this show in terminla?")
+print("This new update i am making in GIT ... will check this on jenkins pipeline")
