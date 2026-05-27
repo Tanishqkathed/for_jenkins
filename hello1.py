8554fb8a0767fc0f510f4246062c749189145bfc
Tanishq kanthed <94047738+Tanishqkathed@users.noreply.github.com>
2026-05-27 15:41:39 +0530

    Add files via upload

---
+++ hello1.py
@@ -0,0 +1,3 @@
+print("Hello I am writing inside CMD ! Anyways Hello from Jenkins ... Tanishq!")
+print("This is updated version .. will this show on cmd ?")
+print("This is another update will this show in terminla?")
---
+++ intro.py
@@ -0,0 +1,3 @@
+print("Hello i am tanishq ! And this program tells you how to add 2 nume")
+print("A + B")
+print(5+5)
